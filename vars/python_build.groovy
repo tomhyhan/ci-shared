0f207fd79b58b762c8e64d7b7f91a027c0cc2114
Tom Han <tomhyhan@gmail.com>
2021-11-27 22:11:39 -0800

    ci

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -26,19 +26,16 @@ def call(dockerRepoName, imageName, app) {
             steps {
                 withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
                     sh "docker scan --login --token 06c99206-5988-411e-860b-b2d63df60346"
-                    sh """
-                    set +e
-                    docker scan --accept-license tomhyhan/${dockerRepoName}:${imageName} --version 
-                    """
-                    // sh "zip app.zip ${app}.txt"
+                    sh "docker scan --accept-license tomhyhan/${dockerRepoName}:${imageName} --version > ${app}.txt "
+                    sh "zip app.zip ${app}.txt"
                 }
             }
-            // post {
-            //     always {
-            //         archiveArtifacts artifacts: 'app.zip', 
-            //         onlyIfSuccessful: true
-            //     }
-            // }
+            post {
+                always {
+                    archiveArtifacts artifacts: 'app.zip', 
+                    onlyIfSuccessful: true
+                }
+            }
         }
     }
     }
